326af73b888c46e035a1ee17189f2d1dca25482f
Yuka Nishijima <48897211+yukanishijima@users.noreply.github.com>
2019-08-25 21:21:49 -0400

    Move styling to style.css

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ let allFriends = [
   {
     name: "Coco",
     photo: "https://images.unsplash.com/photo-1565413294262-fa587c396965?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-    scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    scores: [4, 4, 4, 3, 4, 3, 3, 4, 3, 4]
   },
   {
     name: "Oreo",
     photo: "https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
-    scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+    scores: [2, 5, 5, 5, 5, 2, 4, 2, 4, 3]
   },
   {
     name: "Bella",
     photo: "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80",
-    scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    scores: [3, 2, 1, 1, 3, 3, 2, 2, 2, 1]
   },
   {
     name: "Charlie",
     photo: "https://images.unsplash.com/photo-1544552866-d3ed42536cfd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80",
-    scores: [3, 5, 4, 3, 4, 2, 2, 2, 5, 2]
+    scores: [5, 3, 2, 2, 4, 4, 3, 5, 4, 5]
   }
 ];
 
